865816645b95bd1d44c0ab18dabc3dbfd0474028
Akhil Arun Menon <akhil.menon94@gmail.com>
2026-03-20 22:57:46 +0530

    added models and data

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -74,9 +74,20 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': '@khil2403M',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+            }
     }
 }
 
--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255, db_column='Name')
+    no_of_guests = models.IntegerField(db_column='No_of_guests')
+    booking_date = models.DateTimeField(db_column='BookingDate')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'Booking'
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255, db_column='Title')
+    price = models.DecimalField(max_digits=10, decimal_places=2, db_column='Price')
+    inventory = models.IntegerField(db_column='Inventory')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = 'Menu'
